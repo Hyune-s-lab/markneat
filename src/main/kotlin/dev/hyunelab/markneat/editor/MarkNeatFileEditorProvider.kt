8a3bc81b9979b41ba7db072d7f3eb6a8ae002ff8
Hyune-c <choi8608@gmail.com>
2026-07-11 17:43:38 +0900

    chore: keep theme change focused

--- src/main/kotlin/dev/hyunelab/markneat/editor/MarkNeatFileEditorProvider.kt
+++ src/main/kotlin/dev/hyunelab/markneat/editor/MarkNeatFileEditorProvider.kt
@@ -4,11 +4,10 @@ import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorPolicy
 import com.intellij.openapi.fileEditor.FileEditorProvider
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.jcef.JBCefApp
 
-class MarkNeatFileEditorProvider : FileEditorProvider, DumbAware {
+class MarkNeatFileEditorProvider : FileEditorProvider {
     override fun accept(project: Project, file: VirtualFile): Boolean =
         !file.isDirectory && file.extension?.lowercase() in MARKDOWN_EXTENSIONS
 
